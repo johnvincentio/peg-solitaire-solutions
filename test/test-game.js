d561ed30ca240120f4cbc76f82b636714b6638c3
johnvincentio <john@johnvincent.io>
2018-07-08 17:01:16 -0400

    clean

--- test/test-game.js
+++ test/test-game.js
@@ -14,17 +14,17 @@ var expect = require('chai').expect;
 
 describe('test moves', function() {
 
-    it('test start of the game', function() {
-        let game = new Game(new Board());
-        game.isVictory().should.be.a('boolean');
-        game.isVictory().should.equal(false);
-        game.victories.should.be.equal(0);
+	it('test start of the game', function() {
+		let game = new Game(new Board());
+		game.isVictory().should.be.a('boolean');
+		game.isVictory().should.equal(false);
+		game.victories.should.be.equal(0);
 
-        let from = game.from;
-        from.row.should.be.equal(0);
-        from.column.should.be.equal(0);
-        from.type.should.be.equal(0);
+		let from = game.from;
+		from.row.should.be.equal(0);
+		from.column.should.be.equal(0);
+		from.type.should.be.equal(0);
 
-        game.moves.length.should.be.equal(0);
-    });
+		game.moves.length.should.be.equal(0);
+	});
 });
