6bb8cf84cb9676b649ac2d8e1e36a2178c2c379b
johnvincentio <john@johnvincent.io>
2018-08-21 06:11:04 -0400

    clean

--- test/test-game-victory-2.js
+++ test/test-game-victory-2.js
@@ -17,13 +17,13 @@ const { data1, data2 } = require('./data/data1');
 
 describe('test victory 2', () => {
 	it('test game 1', () => {
-		console.log('data1.length ', data1.length);
-
 		const game = new Game();
 
 		for (let dataIdx = 0; dataIdx < data1.length; dataIdx++) {
 			const obj = data1[dataIdx]; // next move
-			game.handleNextMove(obj);
+			// game.handleNextMove(obj);
+			game.currentMove = obj; // set current move
+			game.handleNextMove(); // make the move
 
 			game.moves.length.should.be.equal(dataIdx + 1);
 
@@ -41,13 +41,13 @@ describe('test victory 2', () => {
 	});
 
 	it('test game 2', () => {
-		console.log('data2.length ', data2.length);
-
 		const game = new Game();
 
 		for (let dataIdx = 0; dataIdx < data2.length; dataIdx++) {
 			const obj = data2[dataIdx]; // next move
-			game.handleNextMove(obj);
+			// game.handleNextMove(obj);
+			game.currentMove = obj; // set current move
+			game.handleNextMove(); // make the move
 
 			game.moves.length.should.be.equal(dataIdx + 1);
 
